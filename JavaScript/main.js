0730af552fa4d3caaac8d342b1ed36fb3c952c19
caellex <157691477+caellex@users.noreply.github.com>
2024-02-07 20:53:38 +0100

    Krokodille spillet

---
+++ JavaScript/main.js
@@ -0,0 +1,29 @@
+
+let showResult =  Math.floor(Math.random() * 100+1);
+let showResult2 =  Math.floor(Math.random() * 100+1);
+const reloadResult = showResult + " er ditt tilfeldige nummer.";
+
+// Første Math.random tall - Kjører math.random og outputter et tall mellom 1-100 til div#mathRDisplay
+showMathRandom();
+function showMathRandom() {
+    document.getElementById('mathRDisplay').innerHTML = /*HTML*/`
+${showResult} er ditt tilfeldige nummer.
+`
+}
+function reloadMathRandom(){
+Math.floor(Math.random() * 100+1);
+showResult = Math.floor(Math.random() * 100+1);
+showMathRandom();
+}
+// Andre Math.random tall - gjør akkurat det samme som over, bare for div#mathRDisplay
+showMathRandom2();
+function showMathRandom2() {
+    document.getElementById('mathRDisplay2').innerHTML = /*HTML*/`
+${showResult2} er ditt tilfeldige nummer.
+`
+}
+function reloadMathRandom2(){
+Math.floor(Math.random() * 100+1);
+showResult2 = Math.floor(Math.random() * 100+1);
+showMathRandom2();
+}
