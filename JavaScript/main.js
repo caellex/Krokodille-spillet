a6935d43ad499adb92f7be5aef733737cf8cf3cb
caellex <157691477+caellex@users.noreply.github.com>
2024-02-08 19:29:56 +0100

    added enter as a valid input key for operator

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -34,21 +34,26 @@ function checkMathCorrect(){
         points++;
         updateMath();
     } else {
-        points--;
-        
+        points--;  
     }
+
     document.getElementById("answer").value = "";
     updateMath();
+}
+
+function handleKeyPress(event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Prevent the default form submission
+        checkMathCorrect(); // Call the function when Enter is pressed
+    }
 }
 
 function failGame(){
     if(points <= -1){
     document.getElementById('gameContainer').innerHTML = /*HTML*/`
-    
     `
     document.getElementById('mainContainer').innerHTML = /*HTML*/`
     <div style="margin: auto auto auto; border: solid black 2px; height: 150px; width: 150px; text-align: center; padding: 5px;"> You failed! Please press CTRL+R to refresh your page and restart. </div>
-
     `
     }
 
